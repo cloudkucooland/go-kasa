4ddeb27be044e846406618bae0ba97c863dd17c0
Scot Bontrager <cloudkucooland@gmail.com>
2025-03-03 22:30:58 -0600

    rewrite this to be not-stupid

--- netutil.go
+++ netutil.go
@@ -2,12 +2,11 @@ package kasa
 
 import (
 	"net"
-	"strings"
 )
 
 // BroadcastAddresses - probably belongs in its own library, get all broadcast addresses
 func BroadcastAddresses() ([]net.IP, error) {
-	var broadcasts []net.IP
+	broadcasts := make([]net.IP, 0, 4)
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		return broadcasts, err
@@ -20,20 +19,23 @@ func BroadcastAddresses() ([]net.IP, error) {
 		}
 
 		for _, addr := range addrs {
-			as := addr.String()
+			switch a := addr.(type) {
+			case *net.IPNet:
+				// skip non-IPv4 and non-loopback
+				v4 := a.IP.To4()
+				if v4 == nil || v4[0] == 127 {
+					continue
+				}
 
-			// ignore IPv6 and loopback since Kasa devices are v4 only (for now)
-			if !strings.Contains(as, ":") && !strings.HasPrefix(as, "127.") {
-				_, ipnet, err := net.ParseCIDR(as)
-				if err != nil {
-					return broadcasts, err
+				for i := 0; i < 4; i++ {
+					v4[i] = v4[i] | ^a.Mask[i]
 				}
-				broadcast := net.IP(make([]byte, 4))
-				for i := range ipnet.IP {
-					broadcast[i] = ipnet.IP[i] | ^ipnet.Mask[i]
-				}
-				broadcasts = append(broadcasts, broadcast)
+				broadcasts = append(broadcasts, v4)
+			default:
+				// skip all non-IP
+				continue
 			}
+
 		}
 	}
 	return broadcasts, nil
