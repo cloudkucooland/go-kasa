17c23f82dd455028d0cb61a4405c368d5389e1f1
Scot Bontrager <cloudkucooland@gmail.com>
2026-03-28 13:57:19 -0500

    chatgpt recommendations

--- netutil.go
+++ netutil.go
@@ -6,31 +6,39 @@ import (
 
 // BroadcastAddresses - probably belongs in its own library, get all broadcast addresses
 func BroadcastAddresses() ([]net.IP, error) {
-	broadcasts := make([]net.IP, 0, 4)
+	broadcasts := make([]net.IP, 0)
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		return broadcasts, err
 	}
 
+	seen := make(map[string]struct{})
+
 	for _, i := range ifaces {
 		addrs, err := i.Addrs()
 		if err != nil {
-			return broadcasts, err
+			continue
+			// return broadcasts, err
 		}
 
 		for _, addr := range addrs {
 			switch a := addr.(type) {
 			case *net.IPNet:
-				// skip non-IPv4 and non-loopback
+				// skip non-IPv4 and loopback
 				v4 := a.IP.To4()
 				if v4 == nil || v4[0] == 127 {
 					continue
 				}
 
-				for i := 0; i < 4; i++ {
-					v4[i] = v4[i] | ^a.Mask[i]
+				for j := 0; j < 4; j++ {
+					v4[j] = v4[j] | ^a.Mask[j]
 				}
-				broadcasts = append(broadcasts, v4)
+
+				key := v4.String()
+				if _, exists := seen[key]; !exists {
+					seen[key] = struct{}{}
+					broadcasts = append(broadcasts, v4)
+				}
 			default:
 				// skip all non-IP
 				continue
