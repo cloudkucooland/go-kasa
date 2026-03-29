61955f28709bf4a1ccc531a071f997ebf90041e5
Scot Bontrager <cloudkucooland@gmail.com>
2026-03-28 22:17:00 -0500

    add stuff

--- commands.go
+++ commands.go
@@ -7,137 +7,119 @@ import (
 	"strings"
 )
 
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
 // SetRelayState changes the relay state of the device -- for multi-relay devices use SetRelayStateChild
 func (d *Device) SetRelayState(newstate bool) error {
-	state := 0
-	if newstate {
-		state = 1
-	}
-	cmd := fmt.Sprintf(CmdSetRelayState, state)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.SetRelayStateCtx(context.Background(), newstate)
+}
+
+func (d *Device) SetRelayStateCtx(ctx context.Context, newstate bool) error {
+	cmd := fmt.Sprintf(CmdSetRelayState, boolToInt(newstate))
+	return d.sendUDP(ctx, cmd)
 }
 
 // SetRelayStateChild adjusts a single relay on a multi-relay device
 func (d *Device) SetRelayStateChild(childID string, newstate bool) error {
-	state := 0
-	if newstate {
-		state = 1
-	}
-	cmd := fmt.Sprintf(CmdSetRelayStateChild, childID, state)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.SetRelayStateChildCtx(context.Background(), childID, newstate)
+}
+
+func (d *Device) SetRelayStateChildCtx(ctx context.Context, childID string, newstate bool) error {
+	cmd := fmt.Sprintf(CmdSetRelayStateChild, childID, boolToInt(newstate))
+	return d.sendUDP(ctx, cmd)
 }
 
 // SetRelayStateChildMulti adjusts multiple relays on a multi-relay device
 func (d *Device) SetRelayStateChildMulti(newstate bool, children ...string) error {
-	state := 0
-	if newstate {
-		state = 1
+	return d.SetRelayStateChildMultiCtx(context.Background(), newstate, children...)
+}
+
+func (d *Device) SetRelayStateChildMultiCtx(ctx context.Context, newstate bool, children ...string) error {
+	quoted := make([]string, len(children))
+	for i, c := range children {
+		quoted[i] = `"` + c + `"`
 	}
 
-	var cc strings.Builder
-	first := true
-	for _, child := range children {
-		if first {
-			first = false
-		} else {
-			cc.WriteRune(44) // ,
-		}
-		cc.WriteRune(34) // "
-		cc.WriteString(child)
-		cc.WriteRune(34) // "
-	}
-	cmd := fmt.Sprintf(CmdSetRelayStateChildMulti, cc.String(), state)
-
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	cmd := fmt.Sprintf(CmdSetRelayStateChildMulti, strings.Join(quoted, ","), boolToInt(newstate))
+	return d.sendUDP(ctx, cmd)
 }
 
 func (d *Device) SendRawCommand(cmd string) error {
-	res, err := d.sendTCP(context.Background(), cmd)
+	return d.SendRawCommandCtx(context.Background(), cmd)
+}
+
+func (d *Device) SendRawCommandCtx(ctx context.Context, cmd string) error {
+	_, err := d.sendTCP(ctx, cmd)
 	if err != nil {
-		klogger.Println(err.Error())
 		return err
 	}
-	klogger.Println(string(res))
 	return nil
 }
 
 // SetBrightness adjust the brightness setting on a dimmer-capable device (1-100)
 func (d *Device) SetBrightness(newval int) error {
+	return d.SetBrightnessCtx(context.Background(), newval)
+}
+
+func (d *Device) SetBrightnessCtx(ctx context.Context, newval int) error {
 	cmd := fmt.Sprintf(CmdSetBrightness, newval)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 func (d *Device) SetFadeOffTime(newval int) error {
+	return d.SetFadeOffTimeCtx(context.Background(), newval)
+}
+
+func (d *Device) SetFadeOffTimeCtx(ctx context.Context, newval int) error {
 	cmd := fmt.Sprintf(CmdSetFadeOffTime, newval)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 func (d *Device) SetFadeOnTime(newval int) error {
+	return d.SetFadeOnTimeCtx(context.Background(), newval)
+}
+
+func (d *Device) SetFadeOnTimeCtx(ctx context.Context, newval int) error {
 	cmd := fmt.Sprintf(CmdSetFadeOnTime, newval)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 func (d *Device) SetGentleOffTime(newval int) error {
+	return d.SetGentleOffTimeCtx(context.Background(), newval)
+}
+
+func (d *Device) SetGentleOffTimeCtx(ctx context.Context, newval int) error {
 	cmd := fmt.Sprintf(CmdSetGentleOffTime, newval)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 func (d *Device) SetGentleOnTime(newval int) error {
+	return d.SetGentleOnTimeCtx(context.Background(), newval)
+}
+
+func (d *Device) SetGentleOnTimeCtx(ctx context.Context, newval int) error {
 	cmd := fmt.Sprintf(CmdSetGentleOnTime, newval)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 // GetSettings gets the device sys info
 func (d *Device) GetSettings() (*Sysinfo, error) {
-	res, err := d.sendTCP(context.Background(), CmdGetSysinfo)
+	return d.GetSettingsCtx(context.Background())
+}
+
+func (d *Device) GetSettingsCtx(ctx context.Context) (*Sysinfo, error) {
+	res, err := d.sendTCP(ctx, CmdGetSysinfo)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
 	var kd KasaDevice
 	if err = json.Unmarshal(res, &kd); err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
@@ -150,15 +132,17 @@ func (d *Device) GetSettings() (*Sysinfo, error) {
 
 // GetEmeter returns emeter data from the device
 func (d *Device) GetEmeter() (*EmeterRealtime, error) {
-	res, err := d.sendTCP(context.Background(), CmdGetEmeter)
+	return d.GetEmeterCtx(context.Background())
+}
+
+func (d *Device) GetEmeterCtx(ctx context.Context) (*EmeterRealtime, error) {
+	res, err := d.sendTCP(ctx, CmdGetEmeter)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
 	var kd KasaDevice
 	if err = json.Unmarshal(res, &kd); err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
@@ -171,18 +155,19 @@ func (d *Device) GetEmeter() (*EmeterRealtime, error) {
 
 // GetEmeterMonth returns a single month's emeter data from the device
 func (d *Device) GetEmeterMonth(month, year int) (*EmeterDaystat, error) {
+	return d.GetEmeterMonthCtx(context.Background(), month, year)
+}
+
+func (d *Device) GetEmeterMonthCtx(ctx context.Context, month, year int) (*EmeterDaystat, error) {
 	q := fmt.Sprintf(CmdEmeterGetMonth, month, year)
 
-	res, err := d.sendTCP(context.Background(), q)
+	res, err := d.sendTCP(ctx, q)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
 	var kd KasaDevice
 	if err = json.Unmarshal(res, &kd); err != nil {
-		klogger.Println(err.Error())
-		klogger.Println(res)
 		return nil, err
 	}
 
@@ -195,18 +180,19 @@ func (d *Device) GetEmeterMonth(month, year int) (*EmeterDaystat, error) {
 
 // GetEmeter returns emeter data from the device
 func (d *Device) GetEmeterChild(child string) (*EmeterRealtime, error) {
+	return d.GetEmeterChildCtx(context.Background(), child)
+}
+
+func (d *Device) GetEmeterChildCtx(ctx context.Context, child string) (*EmeterRealtime, error) {
 	q := fmt.Sprintf(CmdGetEmeterChild, child)
 
-	res, err := d.sendTCP(context.Background(), q)
+	res, err := d.sendTCP(ctx, q)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
 	var kd KasaDevice
 	if err = json.Unmarshal(res, &kd); err != nil {
-		klogger.Println(err.Error())
-		klogger.Println(res)
 		return nil, err
 	}
 
@@ -220,97 +206,102 @@ func (d *Device) GetEmeterChild(child string) (*EmeterRealtime, error) {
 // DisableCloud sets the device to "local only" mode.
 // TODO: forget any cloud settings
 func (d *Device) DisableCloud() error {
-	err := d.sendUDP(context.Background(), CmdCloudUnbind)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.DisableCloudCtx(context.Background())
+}
+
+func (d *Device) DisableCloudCtx(ctx context.Context) error {
+	return d.sendUDP(ctx, CmdCloudUnbind)
 }
 
 // Enable/Configure Cloud
 func (d *Device) EnableCloud(username, password string) error {
-	cmd := fmt.Sprintf(CmdSetServerCreds, username, password)
+	return d.EnableCloudCtx(context.Background(), username, password)
+}
 
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+func (d *Device) EnableCloudCtx(ctx context.Context, username, password string) error {
+	cmd := fmt.Sprintf(CmdSetServerCreds, username, password)
+	return d.sendUDP(ctx, cmd)
 }
 
 // Reboot instructs the device to reboot
 func (d *Device) Reboot() error {
-	err := d.sendUDP(context.Background(), CmdReboot)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.RebootCtx(context.Background())
+}
+
+func (d *Device) RebootCtx(ctx context.Context) error {
+	return d.sendUDP(ctx, CmdReboot)
 }
 
 // SetLEDOff is insanely named... it should be SetLED, but I'm just going with what TP-Link called these things internally...
 func (d *Device) SetLEDOff(t bool) error {
-	off := 0
-	if t {
-		off = 1
-	}
-	cmd := fmt.Sprintf(CmdLEDOff, off)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.SetLEDOffCtx(context.Background(), t)
+}
+
+func (d *Device) SetLEDOffCtx(ctx context.Context, t bool) error {
+	cmd := fmt.Sprintf(CmdLEDOff, boolToInt(t))
+	return d.sendUDP(ctx, cmd)
 }
 
 // SetAlias sets a device name
 func (d *Device) SetAlias(s string) error {
+	return d.SetAliasCtx(context.Background(), s)
+}
+
+func (d *Device) SetAliasCtx(ctx context.Context, s string) error {
 	cmd := fmt.Sprintf(CmdDeviceAlias, s)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 // SetChildAlias sets the name of an individual relay on a multi-relay device, I don't think this works
 func (d *Device) SetChildAlias(childID, s string) error {
+	return d.SetChildAliasCtx(context.Background(), childID, s)
+}
+
+func (d *Device) SetChildAliasCtx(ctx context.Context, childID, s string) error {
 	cmd := fmt.Sprintf(CmdChildAlias, childID, s)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.sendUDP(ctx, cmd)
 }
 
 // SetMode sets the target mode of the system
 func (d *Device) SetMode(m string) error {
+	return d.SetModeCtx(context.Background(), m)
+}
+
+func (d *Device) SetModeCtx(ctx context.Context, m string) error {
 	cmd := fmt.Sprintf(CmdSetMode, m)
-	res, err := d.sendTCP(context.Background(), cmd)
+	res, err := d.sendTCP(ctx, cmd)
 	if err != nil {
-		klogger.Println(err.Error())
 		return err
 	}
-	klogger.Println("SetMode: ", string(res))
 
-	// if err := kd....KasaErr.OK(); err != nil { return nil, err }
+	var kd KasaDevice
+	if err = json.Unmarshal(res, &kd); err != nil {
+		return err
+	}
+
+	if err := kd.GetSysinfo.Sysinfo.KasaErr.OK(); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 // GetWIFIStatus returns the WiFi station info
 func (d *Device) GetWIFIStatus() (*StaInfo, error) {
-	res, err := d.sendTCP(context.Background(), CmdWifiStainfo)
+	return d.GetWIFIStatusCtx(context.Background())
+}
+
+func (d *Device) GetWIFIStatusCtx(ctx context.Context) (*StaInfo, error) {
+	res, err := d.sendTCP(ctx, CmdWifiStainfo)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 	var ksta StaInfo
 	if err := json.Unmarshal(res, &ksta); err != nil {
+		return nil, err
+	}
+
+	if err := ksta.KasaErr.OK(); err != nil {
 		return nil, err
 	}
 
@@ -319,15 +310,21 @@ func (d *Device) GetWIFIStatus() (*StaInfo, error) {
 
 // SetWIFI configures the WiFi station info
 func (d *Device) SetWIFI(ssid string, key string) (*StaInfo, error) {
+	return d.SetWIFICtx(context.Background(), ssid, key)
+}
+
+func (d *Device) SetWIFICtx(ctx context.Context, ssid string, key string) (*StaInfo, error) {
 	cmd := fmt.Sprintf(CmdWifiSetStainfo, ssid, key, 3)
-	res, err := d.sendTCP(context.Background(), cmd)
+	res, err := d.sendTCP(ctx, cmd)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
-	klogger.Println(string(res))
 	var ksta StaInfo
 	if err := json.Unmarshal(res, &ksta); err != nil {
+		return nil, err
+	}
+
+	if err := ksta.KasaErr.OK(); err != nil {
 		return nil, err
 	}
 
@@ -336,13 +333,20 @@ func (d *Device) SetWIFI(ssid string, key string) (*StaInfo, error) {
 
 // GetDimmerParameters returns the dimmer parameters from dimmer-capable devices
 func (d *Device) GetDimmerParameters() (*DimmerParameters, error) {
-	res, err := d.sendTCP(context.Background(), CmdGetDimmer)
+	return d.GetDimmerParametersCtx(context.Background())
+}
+
+func (d *Device) GetDimmerParametersCtx(ctx context.Context) (*DimmerParameters, error) {
+	res, err := d.sendTCP(ctx, CmdGetDimmer)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 	var kd KasaDevice
 	if err := json.Unmarshal(res, &kd); err != nil {
+		return nil, err
+	}
+
+	if err := kd.Dimmer.KasaErr.OK(); err != nil {
 		return nil, err
 	}
 
@@ -351,52 +355,52 @@ func (d *Device) GetDimmerParameters() (*DimmerParameters, error) {
 
 // GetRules returns the rule information from a device
 func (d *Device) GetRules() (string, error) {
-	res, err := d.sendTCP(context.Background(), CmdGetRules)
-	if err != nil {
-		klogger.Println(err.Error())
-		return "", err
-	}
-	return string(res), nil
+	return d.GetRulesCtx(context.Background())
+}
+
+func (d *Device) GetRulesCtx(ctx context.Context) (string, error) {
+	res, err := d.sendTCP(ctx, CmdGetRules)
+	return string(res), err
 }
 
 // GetCountdownRules returns a list of the countdown timers on a device
-func (d *Device) GetCountdownRules() (*[]Rule, error) {
-	res, err := d.sendTCP(context.Background(), CmdGetCountdownRules)
+func (d *Device) GetCountdownRules() ([]Rule, error) {
+	return d.GetCountdownRulesCtx(context.Background())
+}
+
+func (d *Device) GetCountdownRulesCtx(ctx context.Context) ([]Rule, error) {
+	res, err := d.sendTCP(ctx, CmdGetCountdownRules)
 	if err != nil {
-		klogger.Println(err.Error())
 		return nil, err
 	}
 
-	var c KasaDevice
-	if err = json.Unmarshal(res, &c); err != nil {
-		klogger.Println(err.Error())
+	var kd KasaDevice
+	if err = json.Unmarshal(res, &kd); err != nil {
 		return nil, err
 	}
-	return &c.Countdown.GetRules.RuleList, nil
+
+	if err := kd.Countdown.GetRules.OK(); err != nil {
+		return nil, err
+	}
+
+	return kd.Countdown.GetRules.RuleList, nil
 }
 
 // ClearCountdownRules resets all countdown rules on the device
 func (d *Device) ClearCountdownRules() error {
-	err := d.sendUDP(context.Background(), CmdDeleteAllRules)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+	return d.ClearCountdownRulesCtx(context.Background())
+}
+
+func (d *Device) ClearCountdownRulesCtx(ctx context.Context) error {
+	return d.sendUDP(ctx, CmdDeleteAllRules)
 }
 
 // AddCountdownRule adds a new countdown
 func (d *Device) AddCountdownRule(dur int, target bool, name string) error {
-	state := 0
-	if target {
-		state = 1
-	}
+	return d.AddCountdownRuleCtx(context.Background(), dur, target, name)
+}
 
-	cmd := fmt.Sprintf(CmdAddCountdownRule, dur, state, name)
-	err := d.sendUDP(context.Background(), cmd)
-	if err != nil {
-		klogger.Println(err.Error())
-		return err
-	}
-	return nil
+func (d *Device) AddCountdownRuleCtx(ctx context.Context, dur int, target bool, name string) error {
+	cmd := fmt.Sprintf(CmdAddCountdownRule, dur, boolToInt(target), name)
+	return d.sendUDP(ctx, cmd)
 }
---
+++ commands_test.go
@@ -0,0 +1,266 @@
+package kasa_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cloudkucooland/go-kasa"
+)
+
+// ---- Mock Device ----
+
+type mockDevice struct {
+	kasa.Device
+	sendTCPFunc func(ctx context.Context, cmd string) ([]byte, error)
+	sendUDPFunc func(ctx context.Context, cmd string) error
+}
+
+func (m *mockDevice) sendTCP(ctx context.Context, cmd string) ([]byte, error) {
+	if m.sendTCPFunc != nil {
+		return m.sendTCPFunc(ctx, cmd)
+	}
+	return nil, nil
+}
+
+func (m *mockDevice) sendUDP(ctx context.Context, cmd string) error {
+	if m.sendUDPFunc != nil {
+		return m.sendUDPFunc(ctx, cmd)
+	}
+	return nil
+}
+
+// ---- Tests ----
+
+func TestSetRelayStateCtx(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     bool
+		wantCmd   string
+		shouldErr bool
+	}{
+		{"turn on", true, fmt.Sprintf(kasa.CmdSetRelayState, 1), false},
+		{"turn off", false, fmt.Sprintf(kasa.CmdSetRelayState, 0), false},
+		{"udp error", true, fmt.Sprintf(kasa.CmdSetRelayState, 1), true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			called := false
+
+			md := &mockDevice{
+				sendUDPFunc: func(ctx context.Context, cmd string) error {
+					called = true
+					if cmd != tt.wantCmd {
+						t.Fatalf("expected cmd %q, got %q", tt.wantCmd, cmd)
+					}
+					if tt.shouldErr {
+						return errors.New("udp error")
+					}
+					return nil
+				},
+			}
+
+			err := md.SetRelayStateCtx(context.Background(), tt.input)
+
+			if !called {
+				t.Fatal("sendUDP not called")
+			}
+
+			if tt.shouldErr && err == nil {
+				t.Fatal("expected error, got nil")
+			}
+			if !tt.shouldErr && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+		})
+	}
+}
+
+func TestGetSettingsCtx(t *testing.T) {
+	tests := []struct {
+		name      string
+		response  string
+		shouldErr bool
+	}{
+		{
+			name: "valid response",
+			response: `{
+				"system": {
+					"get_sysinfo": {
+						"alias": "plug1",
+						"err_code": 0
+					}
+				}
+			}`,
+			shouldErr: false,
+		},
+		{
+			name: "kasa error",
+			response: `{
+				"system": {
+					"get_sysinfo": {
+						"err_code": -1,
+						"err_msg": "failure"
+					}
+				}
+			}`,
+			shouldErr: true,
+		},
+		{
+			name:      "invalid json",
+			response:  `{invalid}`,
+			shouldErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			md := &mockDevice{
+				sendTCPFunc: func(ctx context.Context, cmd string) ([]byte, error) {
+					if cmd != kasa.CmdGetSysinfo {
+						t.Fatalf("unexpected cmd: %s", cmd)
+					}
+					return []byte(tt.response), nil
+				},
+			}
+
+			res, err := md.GetSettingsCtx(context.Background())
+
+			if tt.shouldErr {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if res == nil {
+				t.Fatal("expected result, got nil")
+			}
+		})
+	}
+}
+
+func TestGetEmeterCtx(t *testing.T) {
+	tests := []struct {
+		name      string
+		response  string
+		shouldErr bool
+	}{
+		{
+			name: "valid",
+			response: `{
+				"emeter": {
+					"get_realtime": {
+						"power_mw": 12345,
+						"err_code": 0
+					}
+				}
+			}`,
+			shouldErr: false,
+		},
+		{
+			name: "device error",
+			response: `{
+				"emeter": {
+					"get_realtime": {
+						"err_code": -1
+					}
+				}
+			}`,
+			shouldErr: true,
+		},
+		{
+			name:      "invalid json",
+			response:  `{invalid}`,
+			shouldErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			md := &mockDevice{
+				sendTCPFunc: func(ctx context.Context, cmd string) ([]byte, error) {
+					if cmd != kasa.CmdGetEmeter {
+						t.Fatalf("unexpected cmd: %s", cmd)
+					}
+					return []byte(tt.response), nil
+				},
+			}
+
+			res, err := md.GetEmeterCtx(context.Background())
+
+			if tt.shouldErr && err == nil {
+				t.Fatal("expected error")
+			}
+			if !tt.shouldErr && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if !tt.shouldErr && res == nil {
+				t.Fatal("expected result")
+			}
+		})
+	}
+}
+
+func TestSetRelayStateChildMultiCtx(t *testing.T) {
+	tests := []struct {
+		name     string
+		children []string
+		state    bool
+	}{
+		{
+			name:     "single child",
+			children: []string{"a"},
+			state:    true,
+		},
+		{
+			name:     "multiple children",
+			children: []string{"a", "b"},
+			state:    false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			md := &mockDevice{
+				sendUDPFunc: func(ctx context.Context, cmd string) error {
+					for _, c := range tt.children {
+						if !strings.Contains(cmd, c) {
+							t.Fatalf("expected child %q in cmd %q", c, cmd)
+						}
+					}
+					return nil
+				},
+			}
+
+			err := md.SetRelayStateChildMultiCtx(context.Background(), tt.state, tt.children...)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+		})
+	}
+}
+
+func TestSendRawCommandCtx(t *testing.T) {
+	md := &mockDevice{
+		sendTCPFunc: func(ctx context.Context, cmd string) ([]byte, error) {
+			if cmd != "test" {
+				t.Fatalf("unexpected cmd: %s", cmd)
+			}
+			return []byte(`ok`), nil
+		},
+	}
+
+	err := md.SendRawCommandCtx(context.Background(), "test")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
